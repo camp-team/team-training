9660ab79c81cc6b82b3ab28fc2cfdffd8adbb373
Hideki Kamakura <kakihk695@gmail.com>
2020-07-26 18:52:41 +0900

    fix(app-module):競合解決　#7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewsComponent } from './news/news.component';
+import { BreadListComponent } from './bread-list/bread-list.component';
 
 @NgModule({
-  declarations: [AppComponent, NewsComponent],
+  declarations: [AppComponent, NewsComponent, BreadListComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],

fe7b09369181bac3e6b1d65afa85dd26582bd147
chamaboy <hiroponpoko07@gmail.com>
2020-07-26 18:01:47 +0900

    feat(footer): form実装 #1

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  lists: string[] = [
+    'Home',
+    'About',
+    'Testimonials',
+    'Discovery',
+    'Help',
+    'Advertise',
+    'Photos',
+    'Terms',
+    'Integrations',
+    'Contact',
+    'Guidlines',
+    'Careers',
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

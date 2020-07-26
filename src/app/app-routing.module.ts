1b7302d08f0802df621fe48d10c566e92579e60d
yuuki-kouno <zgoovw@gmail.com>
2020-07-26 23:27:10 +0900

    fix(ページング): approuting 削除

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PasingComponent } from './pasing/pasing.component';
 
 
 const routes: Routes = [];

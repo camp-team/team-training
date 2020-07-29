3de37b348d67353cee6a6c70d5cd06f41f74a5e6
mitsui <m.mihye1128@gmail.com>
2020-07-29 20:34:29 +0900

    feat(header):drawer開閉実装

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  isOpened = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openMenu() {
+    this.isOpened = true;
+  }
+
+  closeMenu() {
+    this.isOpened = false;
   }
 
 }

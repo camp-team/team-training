f0d4bf7fdcae3f8bfc67735377da1314129f07c1
ReiMeroy <xvwlllwvx@gmail.com>
2020-07-29 23:41:55 +0900

    feat(app.module):comment.card.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TrendingComponent } from './trending/trending.component';
-import { PaginatorComponent } from './paginator/paginator.component';
-import { NewsComponent } from './news/news.component';
 import { BreadListComponent } from './bread-list/bread-list.component';
+import { CommentCardComponent } from './comment-card/comment-card.component';
 import { HeadingComponent } from './heading/heading.component';
 import { HeroComponent } from './hero/hero.component';
+import { NewsComponent } from './news/news.component';
+import { PaginatorComponent } from './paginator/paginator.component';
+import { RelativetimePipe } from './pipe/relativetime.pipe';
+import { TrendingComponent } from './trending/trending.component';
 
 @NgModule({
   declarations: [
@@ -18,10 +19,12 @@ import { HeroComponent } from './hero/hero.component';
     BreadListComponent,
     NewsComponent,
     HeadingComponent,
-    PaginatorComponent
+    PaginatorComponent,
+    RelativetimePipe,
+    CommentCardComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

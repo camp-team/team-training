1a0c96f8a62b201f2d0695f96a223b02b2690eff
ReiMeroy <xvwlllwvx@gmail.com>
2020-07-30 12:48:46 +0900

    feat:comment.ts

--- src/app/comment-card/comment-card.component.ts
+++ src/app/comment-card/comment-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Comment } from '../interfaces/comment';
 
 @Component({
   selector: 'app-comment-card',
@@ -6,33 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comment-card.component.scss'],
 })
 export class CommentCardComponent implements OnInit {
+  @Input() comment: Comment;
   constructor() {}
 
   isPlaying: boolean;
-  dummy: DummyData = {
-    userName: 'Jonathan Walker',
-    avatarUrl: '/assets/comment-card/avatar.png',
-    body:
-      'In hac habitasse platea dictumst. Sed nec venenatis odio. Nulla faucibus ipsum sed faucibus accumsan. Donec rhoncus luctus massa vitae lobortis. Duis consequat, nunc a pretium imperdiet, neque est rhoncus massa, tristique rutrum nisl risus at libero.',
-    thumbnailUrl: '/assets/comment-card/video-thumbnail.png',
-    videoUrl: '/assets/comment-card/video-src-sample.mp4',
-    updatedAt: new Date(),
-  };
-
-  playbtnUrl: string = '/assets/comment-card/playbutton.png';
+  // dummy: DummyData = {
+  //   userName: 'Jonathan Walker',
+  //   avatarUrl: '/assets/comment-card/avatar.png',
+  //   body:
+  //     'In hac habitasse platea dictumst. Sed nec venenatis odio. Nulla faucibus ipsum sed faucibus accumsan. Donec rhoncus luctus massa vitae lobortis. Duis consequat, nunc a pretium imperdiet, neque est rhoncus massa, tristique rutrum nisl risus at libero.',
+  //   thumbnailUrl: '/assets/comment-card/video-thumbnail.png',
+  //   videoUrl: '/assets/comment-card/video-src-sample.mp4',
+  //   updatedAt: new Date(),
+  // };
 
   ngOnInit(): void {}
 
   start() {
     this.isPlaying = true;
   }
-}
-
-export interface DummyData {
-  userName: string;
-  avatarUrl: string;
-  body: string;
-  thumbnailUrl: string;
-  videoUrl: string;
-  updatedAt: Date;
 }
---
+++ src/app/interfaces/comment.ts
@@ -0,0 +1,9 @@
+export interface Comment {
+  id: string;
+  userName: string;
+  avatarUrl: string;
+  body: string;
+  thumbnailUrl: string;
+  videoUrl: string;
+  updatedAt: Date;
+}

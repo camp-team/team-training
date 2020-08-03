b1c43aae1292274ff5e8a6c4dd201fb4d33aa477
peyo-code <mr233425@gmail.com>
2020-08-03 22:59:50 +0900

    feat(small-card): カード小実装

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SmallCardComponent } from './small-card/small-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SmallCardComponent
   ],
   imports: [
     BrowserModule,

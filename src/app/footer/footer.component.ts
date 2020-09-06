e626228d7ea8387c2be612f673cc9f9656332aa2
chamaboy <hiroponpoko07@gmail.com>
2020-09-06 17:25:00 +0900

    feat(footer): 変数追加

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  icons: string[] = ['instagram', 'twitter', 'facebook', 'web'];
+
   constructor() { }
 
   ngOnInit(): void {

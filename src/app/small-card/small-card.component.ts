2c15041efa620c83eed6578ddf2f027e5bbf5ab0
peyo-code <mr233425@gmail.com>
2020-09-06 18:44:43 +0900

    feat(small-card): smallcard実装

---
+++ src/app/small-card/small-card.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  templateUrl: './small-card.component.html',
+  styleUrls: ['./small-card.component.scss']
+})
+export class SmallCardComponent implements OnInit {
+
+
+  constructor() {}
+
+  public readonly cards = [
+    {img: 'assets/images/small01.png', date: '3h ago by Worldnews'},
+    {img: 'assets/images/small02.png', date: '4h ago by Days'},
+    {img: 'assets/images/small03.png', date: '3h ago by Monica'}
+  ];
+
+  ngOnInit(): void {
+  }
+}
